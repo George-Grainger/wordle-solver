41550d7a32af101980380d61db1b1387b6028551
George Grainger <georgegrainger1008@gmail.com>
2023-11-18 11:41:55 +0000

    Added foundational structure to play wordle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+const GAMES: &str = include_str!("../answers.txt");
+
 fn main() {
+    let w = wordle_solver::Wordle::new();
+    for answer in GAMES.split_whitespace() {
+        let guesser = wordle_solver::algorithms::Naive::new();
+        w.play(answer, guesser);
+    }
     println!("Hello, world!");
 }
